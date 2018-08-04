4afa95d4d282b36f18374cb123691094efe650d2
Dannyrobertj <m1075884@moreheadstate.edu>
2018-08-03 22:18:44 -0400

    Fifteenth Commit
    Arranged index file and organized it better

--- js/affix.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-    $('.navbar').affix({
-      offset: {
-        /* Affix the navbar after scroll below header */
-        top: $("header").outerHeight(true)}
-    });
-});
--- js/changeIcon.js
+++
@@ -1,3 +0,0 @@
-$('#menu-toggle').click(function(){
-    $(this).find('i').toggleClass('glyphicon-align-left').toggleClass('glyphicon-remove');
-});
